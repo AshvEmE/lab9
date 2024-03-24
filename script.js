9a19797de36e933204c0841bf3e8eb940d13a95d
AshvEmE <ashveme@gmail.com>
2024-03-24 22:12:34 +0200

    1.5

--- script.js
+++ script.js
@@ -1,49 +1,77 @@
+// Створення рядків з привітаннями на різних мовах
 const str1 = 'Привіт'
 const str2 = 'Hello'
 const str3 = 'Hallo'
 const str4 = 'Ciao'
 const str5 = 'Bonjour'
 
+// Створення результуючого рядка, що об'єднує всі привітання
 const result = `${str1} ${str2} ${str3} ${str4} ${str5}.`
+console.log("Створення результуючого рядка, що об'єднує всі привітання")
+console.log(result)
+console.log('-')
+console.log('-')
+console.log('-')
 
-console.log(result);
+// Перетворення рядка на нижній та верхній регістр
+const lowerCaseStr = str1.toLowerCase()
+const upperCaseStr = str1.toUpperCase()
+console.log('Перетворення рядка на нижній та верхній регістр')
+console.log(lowerCaseStr)
+console.log(upperCaseStr)
+console.log('-')
+console.log('-')
+console.log('-')
 
+// Заміна та видалення зайвих пробілів у рядку
+const str = '  Hello, World!  '
+const replacedStr = str.replace('Hello', 'Привіт')
+const trimmedStr = str.trim()
+console.log('Заміна та видалення зайвих пробілів у рядку')
+console.log(replacedStr)
+console.log(trimmedStr)
+console.log('-')
+console.log('-')
+console.log('-')
 
-const lowerCaseStr = str1.toLowerCase();
-const upperCaseStr = str1.toUpperCase();
-console.log(lowerCaseStr); 
-console.log(upperCaseStr); 
+// Сортування масиву рядків за алфавітом
+const array = ['Банан', 'Апельсин', 'Яблуко']
+array.sort((a, b) => a.localeCompare(b))
+console.log('Сортування масиву рядків за алфавітом')
+console.log(array)
+console.log('-')
+console.log('-')
+console.log('-')
 
+// Перевірка чи містить рядок певну підстрічку
+const strJ = 'JavaScript'
+console.log('Перевірка чи містить рядок певну підстрічку')
+console.log(strJ.includes('JavaScript'))
+console.log(strJ.includes('CSS'))
+console.log('-')
+console.log('-')
+console.log('-')
 
-
-const str = "  Hello, World!  ";
-const replacedStr = str.replace("Hello", "Привіт");
-const trimmedStr = str.trim();
-console.log(replacedStr);
-console.log(trimmedStr);
-
-
-const array = ["Банан", "Апельсин", "Яблуко"];
-array.sort((a, b) => a.localeCompare(b));
-console.log(array);
-
-
-const strJ = "JavaScript";
-console.log(str.includes("JavaScript"));
-console.log(str.includes("CSS")); 
-
-
+// Функція, яка вітає користувачів за їхніми іменами
+console.log(
+	"Функція яка приймає масив об'єктів з даними користувача та буде формувати привітання"
+)
 function welcomeUsers(users) {
 	const greetings = []
 	for (let user of users) {
-		const greeting = `Привіт, ${user.name}! Вітаю вас на нашому сайті!`
-		greetings.push(greeting)
+		greetings.push(`Welcome, ${user.name}!`)
 	}
 	return greetings
 }
+console.log('-')
+console.log('-')
+console.log('-')
 
+// Функція, яка перевіряє, чи є слово паліндромом
+console.log(
+	'Функція яка перевіряє чи слово є паліндромом - для виклику пропишіть isPalindrome'
+)
 function isPalindrome(word) {
 	const reversedWord = word.split('').reverse().join('')
 	return word.toLowerCase() === reversedWord.toLowerCase()
 }
-
